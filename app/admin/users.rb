27d6496ff272fc830d95bd52d97bdd54f4867714
pashko37 <evobbs5@gmail.com>
2018-04-16 16:05:09 +0300

    add admin panel

---
+++ app/admin/users.rb
@@ -0,0 +1,35 @@
+ActiveAdmin.register User do
+  # See permitted parameters documentation:
+   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+   #
+   menu label: 'Users', parent: 'System'
+
+   permit_params :first_name, :last_name, :role
+   #
+   # or
+   #
+   # permit_params do
+   #   permitted = [:permitted, :attributes]
+   #   permitted << :other if params[:action] == 'create' && current_user.admin?
+   #   permitted
+   # end
+
+   index do
+     id_column
+     column :first_name
+     column :last_name
+     actions
+   end
+
+   form do |f|
+     f.inputs do
+       f.input :first_name
+       f.input :last_name
+       # f.input :password
+       # f.input :password_confirmation
+       f.input :role
+     end
+     f.actions
+   end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+end
