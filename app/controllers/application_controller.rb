acd42551c92e3793a34c9efb3f46df3f609595ac
pashko37 <evobbs5@gmail.com>
2018-07-03 18:21:04 +0300

    Add page login and registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def login
+    render "home/login"
+  end
+  def registration
+    render "home/registration"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  
   devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :users, controllers: {  omniauth_callbacks: 'omniauth_callbacks'  } 
-  
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+
   ActiveAdmin.routes(self)
-   get 'welcome/index'
-   root 'welcome#index'
+  get '/login_form', to: 'application#login'
+  get '/registration', to: 'application#registration'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
