4b7eb89692f14c89e2c14454a891650a2f7d7e90
pashko37 <evobbs5@gmail.com>
2018-07-17 10:11:20 +0300

    Update sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   def index
     render "home/index"
   end
+  def register
+    render "home/register"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
   ActiveAdmin.routes(self)
-  
-
   root :to => "application#index"
+  get '/register', to: 'application#register'
+    get '/index', to: 'application#index'
+
 end
